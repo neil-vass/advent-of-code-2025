8e94f7d8286a56528b10f3ce2910940413e4bd13
Neil Vass <neil.vass+github@gmail.com>
2025-12-01 07:50:00 +0000

    Day 1 part 1 done

--- day01/day01.go
+++ day01/day01.go
@@ -1,11 +1,50 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+	"iter"
+	"regexp"
+
+	"github.com/neil-vass/advent-of-code-2025/shared/input"
+)
 
 //go:embed input.txt
 var puzzleData string
 
+type Safe struct{ dial, zeroes int }
+
+var safeRe = regexp.MustCompile(`^(L|R)(\d+)$`)
+
+func (s *Safe) Follow(instructions iter.Seq[string]) {
+	for instr := range instructions {
+		var dir string
+		var dist int
+		err := input.Parse(safeRe, instr, &dir, &dist)
+		if err != nil {
+			panic(err)
+		}
+		if dir == "L" {
+			dist *= -1
+		}
+		s.Turn(dist)
+	}
+}
+
+func (s *Safe) Turn(dist int) {
+	result := (s.dial + dist) % 100
+	if result < 0 {
+		result += 100
+	}
+	s.dial = result
+	if s.dial == 0 {
+		s.zeroes++
+	}
+}
+
 func main() {
-	println("Hello, World!")
-	println(puzzleData)
+	instructions := input.SplitIntoLines(puzzleData)
+	s := Safe{dial: 50}
+	s.Follow(instructions)
+	fmt.Printf("Part 1: %d\n", s.zeroes)
 }
--- day01/day01_test.go
+++ day01/day01_test.go
@@ -1,9 +1,70 @@
 package main
 
-import "testing"
+import (
+	"testing"
 
-func TestGo(t *testing.T) {
-	if 1 != 2 {
-		t.Errorf("Our first test failure")
+	"github.com/neil-vass/advent-of-code-2025/shared/input"
+)
+
+func TestSafeSetup(t *testing.T) {
+	safe := Safe{dial: 50}
+	if safe.dial != 50 || safe.zeroes != 0 {
+		t.Errorf("Safe setup failed")
+	}
+}
+
+func TestSafe_Turn(t *testing.T) {
+	tests := []struct {
+		name    string // description of this test case
+		initial int
+		dist    int
+		want    int
+	}{
+		{name: "Turns right", initial: 11, dist: 8, want: 19},
+		{name: "Turns left", initial: 19, dist: -19, want: 0},
+		{name: "Wraps left", initial: 0, dist: -1, want: 99},
+		{name: "Wraps right", initial: 99, dist: 1, want: 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Safe{dial: tt.initial}
+			s.Turn(tt.dist)
+			if s.dial != tt.want {
+				t.Errorf("Dial position=%d, want %d", s.dial, tt.want)
+			}
+		})
+	}
+}
+
+func TestSafe_FollowInstructions(t *testing.T) {
+	s := Safe{dial: 5}
+	instructions := input.Lines("L10", "R5")
+	s.Follow(instructions)
+	got := s.dial
+	want := 0
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestSafe_CountsZeroes(t *testing.T) {
+	s := Safe{dial: 50}
+	instructions := input.Lines(
+		"L68",
+		"L30",
+		"R48",
+		"L5",
+		"R60",
+		"L55",
+		"L1",
+		"L99",
+		"R14",
+		"L82")
+	s.Follow(instructions)
+	if s.dial != 32 {
+		t.Errorf("Ended at wrong dial position: got %d, want %d", s.dial, 32)
+	}
+	if s.zeroes != 3 {
+		t.Errorf("Wrong zero count: got %d, want %d", s.zeroes, 3)
 	}
 }
