8bd4d7f85b1ae9423fa725d11048bc034f3e8fde
Neil Vass <neil.vass+github@gmail.com>
2025-12-03 07:47:23 +0000

    Day 2 part 1 done

--- day02/day02.go
+++ day02/day02.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
+	"math"
 	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/neil-vass/advent-of-code-2025/shared/input"
 )
 
 var rangeRe = regexp.MustCompile(`^(\d+)-(\d+)$`)
+
+//go:embed input.txt
+var puzzleData string
+
+func main() {
+	puzzleData = strings.TrimSpace(puzzleData)
+	fmt.Printf("Part1: %d\n", Solve(puzzleData))
+}
+
+func Solve(s string) int {
+	total := 0
+	for _, id := range InvalidIDs(s) {
+		total += id
+	}
+	return total
+}
+
+func InvalidIDs(s string) []int {
+	result := []int{}
+	for rangeStr := range strings.SplitSeq(s, ",") {
+		min, max := ParseRange(rangeStr)
+		if IsInvalidID(min) {
+			result = append(result, min)
+		}
+		curr := min
+		for curr < max {
+			// curr = NextInvalidID(curr)
+			// if curr <= max {
+			// 	result = append(result, curr)
+			// }
+			curr++
+			if IsInvalidID(curr) {
+				result = append(result, curr)
+			}
+		}
+	}
+	return result
+}
 
 func ParseRange(rangeStr string) (int, int) {
 	var min, max int
@@ -18,28 +60,31 @@ func ParseRange(rangeStr string) (int, int) {
 	return min, max
 }
 
-func InvalidIDs(s string) []int {
-	result := []int{}
-	for _, rangeStr := range strings.Split(s, ",") {
-		min, max := ParseRange(rangeStr)
-		if isInvalidID(min) {
-			result = append(result, min)
-		}
-		curr := min
-		for curr < max {
-			curr = nextInvalidID(curr)
-			if curr <= max {
-				result = append(result, curr)
-			}
-		}
+func IsInvalidID(n int) bool {
+	digits := strconv.Itoa(n)
+	if len(digits)%2 != 0 {
+		return false
 	}
-	return result
+	half := len(digits) / 2
+	return digits[:half] == digits[half:]
 }
 
-func isInvalidID(min int) bool {
-	return true
-}
+func NextInvalidID(n int) int {
+	digits := strconv.Itoa(n)
+	if len(digits)%2 != 0 {
+		return NextInvalidID(int(math.Pow10(len(digits))))
+	}
+	half := len(digits) / 2
+	left, _ := strconv.Atoi(digits[:half])
+	right, _ := strconv.Atoi(digits[half:])
 
-func nextInvalidID(curr int) int {
-	return 22
+	if left > right {
+		nextID, _ := strconv.Atoi(strconv.Itoa(left) + strconv.Itoa(left))
+		return nextID
+	} else if (left+1)%10 != 0 {
+		nextID, _ := strconv.Atoi(strconv.Itoa(left+1) + strconv.Itoa(left+1))
+		return nextID
+	} else {
+		return NextInvalidID(int(math.Pow10(len(digits) + 1)))
+	}
 }
--- day02/day02_test.go
+++ day02/day02_test.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"strconv"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/neil-vass/advent-of-code-2025/shared/assert"
 )
+
+func TestSolvesExample(t *testing.T) {
+	s := "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+	assert.Equal(t, Solve(s), 1227775554)
+}
 
 func TestInvalidIDs(t *testing.T) {
 	tests := []struct {
@@ -13,6 +20,8 @@ func TestInvalidIDs(t *testing.T) {
 		want []int
 	}{
 		{name: "Ends are invalid", s: "11-22", want: []int{11, 22}},
+		{name: "Invalid inside range", s: "998-1012", want: []int{1010}},
+		{name: "Searches multiple", s: "1188511880-1188511890,222220-222224", want: []int{1188511885, 222222}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -20,6 +29,45 @@ func TestInvalidIDs(t *testing.T) {
 			diff := cmp.Diff(tt.want, got)
 			if diff != "" {
 				t.Errorf("Contents mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestIsInvalidID(t *testing.T) {
+	tests := []struct {
+		n    int
+		want bool
+	}{
+		{n: 11, want: true},
+		{n: 101, want: false},
+		{n: 222220, want: false},
+		{n: 1188511885, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.n), func(t *testing.T) {
+			got := IsInvalidID(tt.n)
+			if got != tt.want {
+				t.Errorf("IsInvalidID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNextInvalidID(t *testing.T) {
+	tests := []struct {
+		n    int
+		want int
+	}{
+		{n: 11, want: 22},
+		{n: 998, want: 1010},
+		{n: 9999, want: 100100},
+	}
+	for _, tt := range tests {
+		t.Run(strconv.Itoa(tt.n), func(t *testing.T) {
+			got := NextInvalidID(tt.n)
+			if got != tt.want {
+				t.Errorf("NextInvalidID() = %v, want %v", got, tt.want)
 			}
 		})
 	}
