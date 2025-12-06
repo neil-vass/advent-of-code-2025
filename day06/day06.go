a5d9ceeb069561a11253af069573df387232d169
Neil Vass <neil.vass+github@gmail.com>
2025-12-06 08:48:24 +0000

    Use []string instead of iter.Seq for input

--- day06/day06.go
+++ day06/day06.go
@@ -6,8 +6,6 @@ import (
 	"iter"
 	"strconv"
 	"strings"
-
-	"github.com/neil-vass/advent-of-code-2025/shared/input"
 )
 
 type nextFn func() int
@@ -16,19 +14,19 @@ type nextFn func() int
 var puzzleData string
 
 func main() {
-	lines := input.SplitIntoLines(puzzleData)
+	splitOnNewline := func(r rune) bool { return r == '\n' }
+	lines := strings.FieldsFunc(puzzleData, splitOnNewline)
 	fmt.Printf("Part 1: %d\n", SolvePart1(lines))
 	//fmt.Printf("Part 2: %d\n", SolvePart2(lines))
 }
 
-func SolvePart1(lines iter.Seq[string]) int {
-	rows := []nextFn{}
-
+func SolvePart1(lines []string) int {
+	numberRows := []nextFn{}
 	var lastStr string
-	for ln := range lines {
+	for _, ln := range lines {
 		if lastStr != "" {
 			next, _ := iter.Pull(strings.FieldsSeq(lastStr))
-			rows = append(rows, func() int {
+			numberRows = append(numberRows, func() int {
 				s, _ := next()
 				n, _ := strconv.Atoi(s)
 				return n
@@ -41,17 +39,21 @@ func SolvePart1(lines iter.Seq[string]) int {
 	for symbol := range strings.FieldsSeq(lastStr) {
 		if symbol == "+" {
 			acc := 0
-			for _, nextVal := range rows {
+			for _, nextVal := range numberRows {
 				acc += nextVal()
 			}
 			total += acc
 		} else {
 			acc := 1
-			for _, nextVal := range rows {
+			for _, nextVal := range numberRows {
 				acc *= nextVal()
 			}
 			total += acc
 		}
 	}
 	return total
+}
+
+func SolvePart2(lines []string) int {
+	return 9
 }
