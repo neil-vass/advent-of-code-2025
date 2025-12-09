4d400f3c57d48852ecda02f9054134cd38644faf
Neil Vass <neil.vass+github@gmail.com>
2025-12-09 07:03:23 +0000

    Day 9 part 1 done

---
+++ day09/day09.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"iter"
+	"regexp"
+
+	"github.com/neil-vass/advent-of-code-2025/shared/input"
+)
+
+type Pos struct{ X, Y int }
+
+//go:embed input.txt
+var puzzleData string
+
+func main() {
+	lines := input.SplitIntoLines(puzzleData)
+	fmt.Printf("Part 1: %d\n", SolvePart1(lines))
+	//fmt.Printf("Part 2: %d\n", SolvePart2(lines))
+}
+
+func SolvePart1(lines iter.Seq[string]) int {
+	corners := ParseTiles(lines)
+	maxArea := 0
+	for i := 0; i < len(corners)-1; i++ {
+		for j := i + 1; j < len(corners); j++ {
+			area := Area(corners[i], corners[j])
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
+
+var tileRe = regexp.MustCompile(`^(\d+),(\d+)$`)
+
+func ParseTiles(lines iter.Seq[string]) []Pos {
+	tiles := []Pos{}
+	for ln := range lines {
+		var p Pos
+		err := input.Parse(tileRe, ln, &p.X, &p.Y)
+		if err != nil {
+			panic(err) // Don't panic!
+		}
+		tiles = append(tiles, p)
+	}
+	return tiles
+}
+
+func Area(a, b Pos) int {
+	length := a.X - b.X
+	if length < 0 {
+		length = -length
+	}
+	length += 1
+	height := a.Y - b.Y
+	if height < 0 {
+		height = -height
+	}
+	height += 1
+	return length * height
+}
---
+++ day09/day09_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/neil-vass/advent-of-code-2025/shared/assert"
+	"github.com/neil-vass/advent-of-code-2025/shared/input"
+)
+
+var example = input.Lines(
+	"7,1",
+	"11,1",
+	"11,7",
+	"9,7",
+	"9,5",
+	"2,5",
+	"2,3",
+	"7,3",
+)
+
+func TestSolvePart1(t *testing.T) {
+	assert.Equal(t, SolvePart1(example), 50)
+}
+
+func TestArea(t *testing.T) {
+	a, b := Pos{2,5}, Pos{9,7}
+	assert.Equal(t, Area(a,b), 24)
+}
