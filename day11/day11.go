ec1298b1a3ff59795a5fc5b92d23bad7e34c6111
Neil Vass <neil.vass+github@gmail.com>
2025-12-11 19:55:30 +0000

    Tried a simple cache, didn't help. Hang on...

--- day11/day11.go
+++ day11/day11.go
@@ -17,12 +17,17 @@ var puzzleData string
 func main() {
 	lines := input.SplitIntoLines(puzzleData)
 	fmt.Printf("Part 1: %d\n", SolvePart1(lines))
-	//fmt.Printf("Part 2: %d\n", Solve(lines, FewestPressesForJoltage))
+	fmt.Printf("Part 2: %d\n", SolvePart2(lines))
 }
 
 func SolvePart1(lines []string) int {
 	graph := ParseGraph(lines)
 	return CountPaths(graph, "you", "out")
+}
+
+func SolvePart2(lines []string) int {
+	graph := ParseGraph(lines)
+	return CountProblemPaths(graph, "svr", "out")
 }
 
 func ParseGraph(lines []string) Graph {
@@ -47,6 +52,47 @@ func CountPaths(graph Graph, start, goal string) int {
 				pathsFound++
 			} else {
 				frontier.Push(connection)
+			}
+		}
+	}
+	return pathsFound
+}
+
+func CountProblemPaths(graph Graph, start, goal string) int {
+	type Path struct {
+		head                 string
+		passedDAC, passedFFT bool
+	}
+
+	pathsFound := 0
+	frontier := fifoqueue.New(Path{start, false, false})
+
+	// Have we been to this point before? How many problem paths did it find?
+	cache := map[Path]int{}
+
+	for !frontier.IsEmpty() {
+		curr := frontier.Pull()
+
+		if pathsFoundLastTimeWeSawThis, ok := cache[curr]; ok {
+			pathsFound += pathsFoundLastTimeWeSawThis
+			continue
+		}
+
+		for _, connection := range graph[curr.head] {
+			if connection == goal {
+				if curr.passedDAC && curr.passedFFT {
+					pathsFound++
+					cache[curr] = 1
+				} else {
+					cache[curr] = 0
+				}
+			} else {
+				updatedPath := Path{
+					head:      connection,
+					passedDAC: curr.passedDAC || connection == "dac",
+					passedFFT: curr.passedFFT || connection == "fft",
+				}
+				frontier.Push(updatedPath)
 			}
 		}
 	}
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -6,19 +6,37 @@ import (
 	"github.com/neil-vass/advent-of-code-2025/shared/assert"
 )
 
-var example = []string{
-	"aaa: you hhh",
-	"you: bbb ccc",
-	"bbb: ddd eee",
-	"ccc: ddd eee fff",
-	"ddd: ggg",
-	"eee: out",
-	"fff: out",
-	"ggg: out",
-	"hhh: ccc fff iii",
-	"iii: out",
+func TestSolvePart1(t *testing.T) {
+	example := []string{
+		"aaa: you hhh",
+		"you: bbb ccc",
+		"bbb: ddd eee",
+		"ccc: ddd eee fff",
+		"ddd: ggg",
+		"eee: out",
+		"fff: out",
+		"ggg: out",
+		"hhh: ccc fff iii",
+		"iii: out",
+	}
+	assert.Equal(t, SolvePart1(example), 5)
 }
 
-func TestSolvePart1(t *testing.T) {
-	assert.Equal(t, SolvePart1(example), 5)
+func TestSolvePart2(t *testing.T) {
+	example := []string{
+		"svr: aaa bbb",
+		"aaa: fft",
+		"fft: ccc",
+		"bbb: tty",
+		"tty: ccc",
+		"ccc: ddd eee",
+		"ddd: hub",
+		"hub: fff",
+		"eee: dac",
+		"dac: fff",
+		"fff: ggg hhh",
+		"ggg: out",
+		"hhh: out",
+	}
+	assert.Equal(t, SolvePart2(example), 2)
 }
