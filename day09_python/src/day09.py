ce843fdfa34f38d22b3ee5e4b65bfc78557a0db4
Neil Vass <neil.vass+github@gmail.com>
2025-12-17 21:16:18 +0000

    Tidy

--- day09_python/src/day09.py
+++ day09_python/src/day09.py
@@ -1,7 +1,11 @@
 
 import math
-
 import numpy as np
+
+class Pos:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 class Rectangle:
     def __init__(self, min_pos, max_pos):
@@ -9,8 +13,8 @@ class Rectangle:
         self.max_pos = max_pos
 
 def area(rect):
-    return ((rect.max_pos[0] - rect.min_pos[0] + 1) *
-            (rect.max_pos[1] - rect.min_pos[1] + 1))
+    return ((rect.max_pos.x - rect.min_pos.x + 1) *
+            (rect.max_pos.y - rect.min_pos.y + 1))
 
 class Polygon:
     def  __init__(self, vertices, min_bound, max_bound, candidates):
@@ -21,13 +25,13 @@ class Polygon:
         self.mark_outside()
     
     def mark_outside(self):
-        self.outside = np.zeros((self.max_bound[0]+3, self.max_bound[1]+3), dtype=bool)
+        self.outside = np.zeros((self.max_bound.x+3, self.max_bound.y+3), dtype=bool)
 
         # Make clockwise.
         signed_area = 0
         for i in range(len(self.vertices) - 1):
             start, end = self.vertices[i], self.vertices[i+1]
-            signed_area += (start[0]*end[1] - end[0]*start[1])
+            signed_area += (start.x*end.y - end.x*start.y)
 	
         if signed_area > 0:
             self.vertices.reverse()
@@ -35,41 +39,41 @@ class Polygon:
         # Mark the "you're stepping outside" border.
         for i in range(len(self.vertices) - 1):
             edge = (self.vertices[i], self.vertices[i+1])
-            if edge[0][0] == edge[1][0]: # is_horizontal
-                if edge[0][1] < edge[1][1]:
-                    x = edge[0][0] -1
-                    y_start = edge[0][1]
-                    y_end = edge[1][1] +1
+            if edge[0].x == edge[1].x: # is_horizontal
+                if edge[0].y < edge[1].y:
+                    x = edge[0].x -1
+                    y_start = edge[0].y
+                    y_end = edge[1].y +1
                     self.outside[x, y_start:y_end] = True
                 else:
-                    x = edge[0][0] +1
-                    y_start = edge[1][1]
-                    y_end = edge[0][1] +1
+                    x = edge[0].x +1
+                    y_start = edge[1].y
+                    y_end = edge[0].y +1
                     self.outside[x, y_start:y_end] = True
             else:
-                if edge[0][0] < edge[1][0]:
-                    y = edge[0][1] +1
-                    x_start = edge[0][0]
-                    x_end = edge[1][0] +1
+                if edge[0].x < edge[1].x:
+                    y = edge[0].y +1
+                    x_start = edge[0].x
+                    x_end = edge[1].x +1
                     self.outside[x_start:x_end, y] = True
                 else:
-                    y = edge[0][1] -1
-                    x_start = edge[1][0]
-                    x_end = edge[0][0] +1
+                    y = edge[0].y -1
+                    x_start = edge[1].x
+                    x_end = edge[0].x +1
                     self.outside[x_start:x_end, y] = True
 
         # Remove edges that got mistakenly marked as "outside" above.
         for i in range(len(self.vertices) - 1):
             edge = (self.vertices[i], self.vertices[i+1])
-            if edge[0][0] == edge[1][0]: # is_horizontal
-                x = edge[0][0]
-                y_start = min(edge[0][1], edge[1][1])
-                y_end = max(edge[0][1], edge[1][1]) +1
+            if edge[0].x == edge[1].x: # is_horizontal
+                x = edge[0].x
+                y_start = min(edge[0].y, edge[1].y)
+                y_end = max(edge[0].y, edge[1].y) +1
                 self.outside[x, y_start:y_end] = False
             else:
-                y = edge[0][1]
-                x_start = min(edge[0][0], edge[1][0])
-                x_end = max(edge[0][0], edge[1][0]) +1
+                y = edge[0].y
+                x_start = min(edge[0].x, edge[1].x)
+                x_end = max(edge[0].x, edge[1].x) +1
                 self.outside[x_start:x_end, y] = False			
 
 
@@ -80,10 +84,10 @@ class Polygon:
 
     def solve_part_2(self):
         for i, c in enumerate(self.candidates):
-            if not (self.outside[c.min_pos[0], c.min_pos[1]:c.max_pos[1]+1].any() or
-                    self.outside[c.max_pos[0], c.min_pos[1]:c.max_pos[1]+1].any() or
-                    self.outside[c.min_pos[0]:c.max_pos[0]+1, c.min_pos[1]].any() or
-                    self.outside[c.min_pos[0]:c.max_pos[0]+1, c.max_pos[1]].any()):
+            if not (self.outside[c.min_pos.x, c.min_pos.y:c.max_pos.y+1].any() or
+                    self.outside[c.max_pos.x, c.min_pos.y:c.max_pos.y+1].any() or
+                    self.outside[c.min_pos.x:c.max_pos.x+1, c.min_pos.y].any() or
+                    self.outside[c.min_pos.x:c.max_pos.x+1, c.max_pos.y].any()):
                 return area(c)
 
         raise ValueError("No suitable rectangles at all")
@@ -92,17 +96,18 @@ class Polygon:
 def parse_polygon(lines):
     tiles = []
     rectanges = []
-    min_bound = [math.inf, math.inf]
-    max_bound = [-math.inf, -math.inf]
+    min_bound = Pos(math.inf, math.inf)
+    max_bound = Pos(-math.inf, -math.inf)
 
     for ln in lines:
-        tile = [int(n)+1 for n in ln.split(',')]
-        min_bound = [min(min_bound[0], tile[0]), min(min_bound[1], tile[1])]
-        max_bound = [max(max_bound[0], tile[0]), max(max_bound[1], tile[1])]
+        nums = [int(n)+1 for n in ln.split(',')]
+        tile = Pos(x=nums[0], y=nums[1])
+        min_bound = Pos(min(min_bound.x, tile.x), min(min_bound.y, tile.y))
+        max_bound = Pos(max(max_bound.x, tile.x), max(max_bound.y, tile.y))
 
         for otherTile in tiles:
-            min_pos = [min(tile[0], otherTile[0]), min(tile[1], otherTile[1])]
-            max_pos = [max(tile[0], otherTile[0]), max(tile[1], otherTile[1])]
+            min_pos = Pos(min(tile.x, otherTile.x), min(tile.y, otherTile.y))
+            max_pos = Pos(max(tile.x, otherTile.x), max(tile.y, otherTile.y))
             rectanges.append(Rectangle(min_pos, max_pos))
 		
         tiles.append(tile)
